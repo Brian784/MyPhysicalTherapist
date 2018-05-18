0c37c76c3a3c846b313bf096a0f7c017c5497d6e
Ryan Conner <Brianxu784@gmail.com>
2018-05-18 12:34:35 +0800

    ver2

--- user/videosbackup.php
+++ user/videosbackup.php
@@ -67,29 +67,22 @@ if($pageCntentRange1<0){
     $pageCntentRange1=0;
 }
 $pageCntentRange2 = $_GET['page'] * $itemperpage;
-$isSearch=null;
-if(isset($_GET['part'])) {
-    switch (strtolower($_GET['part'])) {
-        case 'upper':
-            $isSearch = false;
-            $sql = 'SELECT b.Therapist_ID,a.Video_ID,a.Video_Title,SUBSTRING(a.Video_Description,1,300) as Video_Description,a.Video_URL,b.First_Name,b.Last_Name FROM video_library a INNER JOIN therapist_account b WHERE Body_Part=\'upper\' AND a.Therapist_ID=b.Therapist_ID AND b.isValidated =1 AND a.Video_ID >= ' . $pageCntentRange1 . ' AND  a.Video_ID <' . $pageCntentRange2 . ' ORDER BY a.Video_ID DESC';
-            break;
-        case 'lower':
-            $isSearch = false;
-            $sql = 'SELECT b.Therapist_ID,a.Video_ID,a.Video_Title,SUBSTRING(a.Video_Description,1,300) as Video_Description,a.Video_URL,b.First_Name,b.Last_Name FROM video_library a INNER JOIN therapist_account b WHERE Body_Part=\'lower\' AND a.Therapist_ID=b.Therapist_ID AND b.isValidated =1 AND a.Video_ID >= ' . $pageCntentRange1 . ' AND a.Video_ID < ' . $pageCntentRange2 . ' ORDER BY a.Video_ID DESC';
-            break;
-        default:
-            $isSearch = true;
-            $sql = 'SELECT b.Therapist_ID,a.Video_ID,a.Video_Title,SUBSTRING(a.Video_Description,1,300) as Video_Description,a.Video_URL,b.First_Name,b.Last_Name FROM video_library a INNER JOIN therapist_account b WHERE a.Therapist_ID=b.Therapist_ID AND b.isValidated =1 AND a.Video_Title LIKE \'%' . $_GET['part'] . '%\' ORDER BY a.Video_ID DESC';
-            break;
-    }
 
-    $dbConn->setQuery($sql);
-    $result = $dbConn->executeSelectQuery();
-}else{
-    header('Location: index.php');
-    die();
+switch (strtolower($_GET['part'])) {
+    case 'upper':
+        $sql = 'SELECT b.Therapist_ID,a.Video_ID,a.Video_Title,SUBSTRING(a.Video_Description,1,300) as Video_Description,a.Video_URL,b.First_Name,b.Last_Name FROM video_library a INNER JOIN therapist_account b WHERE Body_Part=\'upper\' AND a.Therapist_ID=b.Therapist_ID AND b.isValidated =1 AND a.Video_ID >= '.$pageCntentRange1.' AND  a.Video_ID <'.$pageCntentRange2.' ORDER BY a.Video_ID DESC';
+        break;
+    case 'lower':
+        $sql = 'SELECT b.Therapist_ID,a.Video_ID,a.Video_Title,SUBSTRING(a.Video_Description,1,300) as Video_Description,a.Video_URL,b.First_Name,b.Last_Name FROM video_library a INNER JOIN therapist_account b WHERE Body_Part=\'lower\' AND a.Therapist_ID=b.Therapist_ID AND b.isValidated =1 AND a.Video_ID >= '.$pageCntentRange1.' AND a.Video_ID < '.$pageCntentRange2.' ORDER BY a.Video_ID DESC';
+        break;
+    default:
+        header('location:index.php');
+        die();
 }
+
+$dbConn->setQuery($sql);
+$result = $dbConn->executeSelectQuery();
+
 ?>
 
 
@@ -180,124 +173,62 @@ if(isset($_GET['part'])) {
     </div>
 
 </div>
-<!-- Page Content -->
 <div class="container">
 
+    <h1 class="my-4"><?php echo ucfirst($_GET['part']) . ' Body'; ?></h1>
     <div class="row">
-
-
-        <!-- Blog Entries Column -->
-        <div class="col-md-8">
-            <?php if(!$isSearch){ ?>
-            <h1 class="my-4"><?php echo ucfirst($_GET['part']).' Body';?>
-            </h1>
-            <?php }else{ ?>
-                <h1 class="my-4"><?php echo 'Searched \''.ucfirst($_GET['part']).'\'';?>
-                </h1>
-            <?php } ?>
-            <!-- Blog Post -->
-            <?php if($result->num_rows>0){?>
+        <?php if($result->num_rows>0){?>
             <?php while ($row = @mysqli_fetch_array($result)) { ?>
-            <div class="card mb-4">
-             <!--   <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">-->
-                <div class="card-body">
-                    <h2 class="card-title"><?php echo $row['Video_Title']?></h2>
-                    <form id="video<?php echo $row['Video_ID'] ?>" action="playvideo.php" method="get">
-                        <input type="hidden" name="videoID" value="<?php echo $row['Video_ID'] ?>">
-                    </form>
-                    <form id="therapist<?php echo $row['Therapist_ID'] ?>" action="therapistprofile.php"
-                          method="get">
-                        <input type="hidden" name="therapistID" value="<?php echo $row['Therapist_ID'] ?>">
-                    </form>
-                    <p class="card-text"> <?php echo $row['Video_Description']?></p>
-                    <a onclick="document.getElementById('video<?php echo $row['Video_ID'] ?>' ).submit();" class="btn btn-primary">Watch &rarr;</a>
+                <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <h4 class="card-title">
+                                <form id="video<?php echo $row['Video_ID'] ?>" action="playvideo.php" method="get">
+                                    <input type="hidden" name="videoID" value="<?php echo $row['Video_ID'] ?>">
+                                </form>
+
+                                <a onclick="document.getElementById('video<?php echo $row['Video_ID'] ?>' ).submit();"><?php echo $row['Video_Title'] ?></a>
+                            </h4>
+                            <form id="therapist<?php echo $row['Therapist_ID'] ?>" action="therapistprofile.php"
+                                  method="post">
+                                <input type="hidden" name="therapistID" value="<?php echo $row['Therapist_ID'] ?>">
+                            </form>
+
+                            <h5><span class="glyphicon glyphicon-user">
+                    </span> Post by
+                                <a onclick="document.getElementById('therapist<?php echo $row['Therapist_ID'] ?>' ).submit();"><?php echo $row['First_Name'] . '  ' . $row['Last_Name'] ?></a>
+                            </h5>
+                            <p class="card-text">
+                                <?php echo $row['Video_Description'] ?>
+                            </p>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-footer text-muted">
-                    <span class="glyphicon glyphicon-user"></span>
-                    Posted by
-                    <a onclick="document.getElementById('therapist<?php echo $row['Therapist_ID'] ?>' ).submit();"><?php echo $row['First_Name'] . '  ' . $row['Last_Name'] ?></a>
-                </div>
-            </div>
-                <?php } ?>
-            <?php }else{?>
-                <h1>No Contents</h1>
+
             <?php } ?>
+        <?php }else{?>
+<h1>No Contents</h1>
+        <?php } ?>
         </div>
-        <!-- Sidebar Widgets Column -->
-        <div class="col-md-4">
-
-            <!-- Search Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Search</h5>
-                <div class="card-body">
-                    <div class="input-group">
-                        <form id="searchForm" action="videos.php" method="get">
-                        <input type="text" name='part' class="form-control" placeholder="Search for...">
-                        </form>
-                        <span class="input-group-btn">
-                  <button class="btn btn-secondary" type="button" onclick="document.getElementById('searchForm').submit();">Go!</button>
-                </span>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Categories Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Categories</h5>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="?part=upper">Upper Body</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-6">
-                            <ul class="list-unstyled mb-0">
-                                <li>
-                                    <a href="?part=lower">Lower Body</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Side Widget -->
-            <div class="card my-4">
-                <h5 class="card-header">Side Widget</h5>
-                <div class="card-body">
-                    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-    <!-- /.row -->
-
-
 
     <ul class="pagination justify-content-center">
 
         <?php
-if(!$isSearch) {
-    foreach (range(1, $totalPages) as $page) {
-        // Check if we're on the current page in the loop
 
-        if ($page == $_GET['page']) {
-            echo '<li class="page-item active">';
-            echo '<a class="page-link" href="?page=' . $page . '?part=' . $_GET['part'] . '">' . $page . '</a>';
-            echo '</li>';
-        } else if ($page == 1 || $page == $totalPages || ($page >= $_GET['page'] - 2 && $page <= $_GET['page'] + 2)) {
-            echo '   <li class="page-item ">
-            <a class="page-link" href="?page=' . $page . '?part=' . $_GET['part'] . '">' . $page . '</a>
+        foreach (range(1, $totalPages) as $page) {
+            // Check if we're on the current page in the loop
+
+            if ($page == $_GET['page']) {
+                echo '<li class="page-item active">';
+                echo '<a class="page-link" href="?page=' . $page . '?part='.$_GET['part'].'">' . $page . '</a>';
+                echo '</li>';
+            } else if ($page == 1 || $page == $totalPages || ($page >= $_GET['page'] - 2 && $page <= $_GET['page'] + 2)) {
+                echo '   <li class="page-item ">
+            <a class="page-link" href="?page=' . $page . '?part='.$_GET['part'].'">' . $page . '</a>
         </li>';
-        }
+            }
 
-    }
-}
+        }
         ?>
 
     </ul>
